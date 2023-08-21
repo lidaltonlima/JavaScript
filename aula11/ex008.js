38d6fbb5041a64db73366443936d375f7cbd58d8
lidalton <lidaltons@gmail.com>
2023-08-21 13:07:10 -0300

    crate: aula11

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 58
+console.log(`Your velocity is ${vel} km/h`)
+
+if (vel > 60) { // Condição simples
+    console.log('Você outrapassou a velcidade limite. MULTADO!')
+}
+
+console.log('Use o cinto de segurança')
